31878017057cbaf3e3e781722439d0462febd1d8
TurtIeSocks <TurtIeSocks@users.noreply.github.com>
2026-06-01 20:30:13 +0000

    deploy: 31de12cabcca7a80b6489f5e0213e07beab1f4e1

--- api/trait.impl/core/ops/drop/trait.Drop.js
+++ api/trait.impl/core/ops/drop/trait.Drop.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["zendriver_interception",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.95.0/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"zendriver_interception/actor/struct.InterceptHandle.html\" title=\"struct zendriver_interception::actor::InterceptHandle\">InterceptHandle</a>",0],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.95.0/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"zendriver_interception/paused/struct.PausedRequest.html\" title=\"struct zendriver_interception::paused::PausedRequest\">PausedRequest</a>",0]]]]);
+    const implementors = Object.fromEntries([["zendriver",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.96.0/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"zendriver/browser_context/struct.BrowserContext.html\" title=\"struct zendriver::browser_context::BrowserContext\">BrowserContext</a>",0]]],["zendriver_interception",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.96.0/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"zendriver_interception/actor/struct.InterceptHandle.html\" title=\"struct zendriver_interception::actor::InterceptHandle\">InterceptHandle</a>",0],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.96.0/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"zendriver_interception/paused/struct.PausedRequest.html\" title=\"struct zendriver_interception::paused::PausedRequest\">PausedRequest</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[670]}
+//{"start":59,"fragment_lengths":[329,671]}
