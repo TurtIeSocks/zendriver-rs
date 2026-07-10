ad854f8d5dc7f1b2460ea01f66c96641dca71d13
TurtIeSocks <TurtIeSocks@users.noreply.github.com>
2026-07-10 15:28:44 +0000

    deploy: 81e24ca2a19bcfff5d37e6c7debc65273acc2c41

--- api/trait.impl/core/convert/trait.TryFrom.js
+++ api/trait.impl/core/convert/trait.TryFrom.js
@@ -1,5 +1,5 @@
 (function() {
-    const implementors = Object.fromEntries([["zendriver_stealth",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.96.0/core/convert/trait.TryFrom.html\" title=\"trait core::convert::TryFrom\">TryFrom</a>&lt;&amp;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.96.0/std/primitive.str.html\">str</a>&gt; for <a class=\"struct\" href=\"zendriver_stealth/geo/struct.Country.html\" title=\"struct zendriver_stealth::geo::Country\">Country</a>",0]]]]);
+    const implementors = Object.fromEntries([["zendriver_stealth",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.97.0/core/convert/trait.TryFrom.html\" title=\"trait core::convert::TryFrom\">TryFrom</a>&lt;&amp;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.97.0/std/primitive.str.html\">str</a>&gt; for <a class=\"struct\" href=\"zendriver_stealth/geo/struct.Country.html\" title=\"struct zendriver_stealth::geo::Country\">Country</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
