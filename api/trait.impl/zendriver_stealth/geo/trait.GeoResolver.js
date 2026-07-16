7e04841c547ac988af95ec98b5811656777286c4
TurtIeSocks <TurtIeSocks@users.noreply.github.com>
2026-07-16 21:38:56 +0000

    deploy: e3be43f0b087bbf8021f6e0a77fef21a9b6994ce

---
+++ api/trait.impl/zendriver_stealth/geo/trait.GeoResolver.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["zendriver",[["impl GeoResolver for <a class=\"struct\" href=\"zendriver/struct.IpApiResolver.html\" title=\"struct zendriver::IpApiResolver\">IpApiResolver</a>",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[167]}
