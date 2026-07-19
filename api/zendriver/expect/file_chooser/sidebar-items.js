c0bd8de9d48361c57d95711b7b375161657cf35e
TurtIeSocks <TurtIeSocks@users.noreply.github.com>
2026-07-19 13:25:46 +0000

    deploy: da3895181d87daddb2369dbb77010e601ae2d626

---
+++ api/zendriver/expect/file_chooser/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["FileChooserMode"],"struct":["FileChooserExpectation","MatchedFileChooser"]};
--- api/zendriver/expect/sidebar-items.js
+++ api/zendriver/expect/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["UrlMatcher"],"mod":["dialog","download","request","response"]};
+window.SIDEBAR_ITEMS = {"enum":["UrlMatcher"],"mod":["dialog","download","file_chooser","request","response"]};
